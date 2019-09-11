d3564d4100118b49a82a75c38cfa6ee76b4c89d9
Abhishu Mukherjee <abhishu.m@utexas.edu>
2019-09-10 21:16:01 -0500

    adding random # generators

--- assets/javascrip/game.js
+++ assets/javascrip/game.js
@@ -1,12 +1,49 @@
+$(document).ready(function () {
 var wins = 0;
 var losses = 0;
 var total = 0;
 
-$(document).ready(function () {
+var num1 = Math.floor(Math.random() * 9) + 1;
+    console.log(num1)
+var num2 = Math.floor(Math.random() * 9) + 1;
+    console.log(num2)
+var num3 = Math.floor(Math.random() * 9) + 1;
+    console.log(num3)
+var num4 = Math.floor(Math.random() * 9) + 1;
+    console.log(num4)
 
-    var randomNumber = Math.floor(Math.random() * 102) + 19;
+function winning() {
+    wins++;
+    $("#wins").text("Wins: " + wins);
+    replay()
+}
+function losing() {
+    wins++;
+    $("#losses").text("Losses: " + losses);
+    replay()
+}
+
+
+var randomNumber = Math.floor(Math.random() * 102) + 19;
     console.log(randomNumber);
     $("#randomNumber").text(randomNumber);
+
+$(".one").on("click", function () {
+
+    });
+
+$(".two").on("click", function () {
+    
+    });
+    
+$(".three").on("click", function () {
+    
+    });
+    
+$(".four").on("click", function () {
+        
+    });
 });
+
 
 
